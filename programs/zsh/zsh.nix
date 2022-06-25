97b8550c96c71d70025447451830c50d7a69c3ba
James D <james@integratn.io>
2022-06-25 15:07:23 -0600

    succeeded in moving plugins. yay

--- programs/zsh/zsh.nix
+++ programs/zsh/zsh.nix
@@ -3,49 +3,7 @@
   programs.zsh = with pkgs; {
     enable = true;
     oh-my-zsh.enable = true;
-    plugins = import ./plugins.nix { inherit pkgs; }
-    # plugins = with pkgs; [
-    #   {
-    #     name = "zsh-nix-shell";
-    #     file = "nix-shell.plugin.zsh";
-    #     src = pkgs.fetchFromGitHub {
-    #       owner = "chisui";
-    #       repo = "zsh-nix-shell";
-    #       rev = "v0.4.0";
-    #       sha256 = "037wz9fqmx0ngcwl9az55fgkipb745rymznxnssr3rx9irb6apzg";
-    #     };
-    #   }
-    #   {
-    #     name = "zsh-syntax-highlighting";
-    #     src = pkgs.fetchFromGitHub {
-    #       owner = "zsh-users";
-    #       repo = "zsh-syntax-highlighting";
-    #       rev = "0.7.1";
-    #       sha256 = "gOG0NLlaJfotJfs+SUhGgLTNOnGLjoqnUp54V9aFJg8=";
-    #     };
-    #     file = "zsh-syntax-highlighting.zsh";
-    #   }
-    #   {
-    #     name = "zsh-autosuggestions";
-    #     src = pkgs.fetchFromGitHub {
-    #       owner = "zsh-users";
-    #       repo = "zsh-autosuggestions";
-    #       rev = "v0.7.0";
-    #       sha256 = "KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
-    #     };
-    #     file = "zsh-autosuggestions.zsh";
-    #   }
-    #   {
-    #     name = "zsh-history-substring-search";
-    #     src = pkgs.fetchFromGitHub {
-    #       owner = "zsh-users";
-    #       repo = "zsh-history-substring-search";
-    #       rev = "v1.0.2";
-    #       sha256 = "Ptxik1r6anlP7QTqsN1S2Tli5lyRibkgGlVlwWZRG3k=";
-    #     };
-    #     file = "zsh-history-substring-search.zsh";
-    #   }
-    # ];
+    plugins = import ./plugins.nix { inherit pkgs; };
     localVariables = {};
     dotDir = ".config/zsh";
     enableAutosuggestions = true;
