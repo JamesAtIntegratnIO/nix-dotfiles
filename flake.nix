162e1560e7a8ac06b81b9f1faaf4d60ad905f99a
James D <james@integratn.io>
2023-01-07 13:04:56 -0700

    initial rebuild

--- flake.nix
+++ flake.nix
@@ -1,79 +1,44 @@
 {
-  description = "Home Manager configs";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    # nixgl is needed for alacritty outside of nixOS
-    # refer to https://github.com/NixOS/nixpkgs/issues/122671
-    # https://github.com/guibou/nixGL/#use-an-overlay
-    nixgl.url = "github:guibou/nixGL";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    flake-utils.url = "github:numtide/flake-utils";
+    vscode-server.url = "github:msteen/nixos-vscode-server";
   };
 
-  outputs = { self, nixpkgs, home-manager, nixgl, ... }:
-    let
-      pkgsForSystem = system: import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-        };
-        overlays = [ nixgl.overlay ];
+  outputs = inputs@{ self, nixpkgs, home-manager, flake-utils, nixpkgs-stable, vscode-server, ... }: rec {
+    overlays = {};
+    nixosConfigurations = {
+      devvm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./system/dev-nixos-vm/configuration.nix
+          vscode-server.nixosModule
+          ({ config, pkgs, ... }: {
+            services.vscode-server.enable = true;
+          })
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.boboysdadda = (
+              { config, pkgs, ... }:
+              {
+                home.stateVersion = "20.09";
+                targets.genericLinux.enable = true;
+                imports = [
+                  ./personal.nix
+                ];
+              }
+            );
+            
+          }
+        ];
       };
-      # Inspired by https://github.com/jonringer/nixpkgs-config/blob/master/flake.nix#L32-L38
-      # Also showed me how to get access to the pkgs.stdenv isLinux isDarwin
-      mkHomeConfiguration = args: home-manager.lib.homeManagerConfiguration (rec {
-          username = args.username;
-          system = args.system or "x86_64-linux";
-          pkgs = pkgsForSystem system;
-          homeDirectory = args.homeDirectory;
-          configuration = { pkgs, ... }: {
-            imports = [ ./home.nix ];
-          };
-          stateVersion = "22.11";
-        }// args);
-    in 
-   {
-    inherit home-manager;
-
-    homeConfigurations = {
-      
-      personal = mkHomeConfiguration rec {
-        username = "boboysdadda";
-        homeDirectory = "/home/${username}";
-        extraSpecialArgs = {
-          withGUI = true;
-          font = "FiraCode Nerd Font Mono";
-          fontSize = 14;
-          homeDirectory = "/home/boboysdadda";
-        };
-      };
-      
-      macJames = mkHomeConfiguration rec {
-        username = "james";
-        system = "x86_64-darwin";
-        homeDirectory = "/Users/${username}";
-        extraSpecialArgs = {
-          withGUI = true;
-          font = "FiraCode Nerd Font Mono";
-          fontSize = 14;
-          homeDirectory = "/Users/james";
-        };
-      };
-
-      rhJames = mkHomeConfiguration rec {
-        username = "james";
-        homeDirectory = "/home/${username}";
-        extraSpecialArgs = {
-          withGUI = true;
-          font = "FiraCode Nerd Font Mono";
-          fontSize = 14;
-          homeDirectory = "/home/james";
-        };
-      };
-      
     };
   };
 }
