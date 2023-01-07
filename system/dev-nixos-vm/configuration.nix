40977b194318eaefe7f9b7a097d0fd7cca4ed80d
James D <james@integratn.io>
2023-01-07 14:10:53 -0700

    set hostname

--- system/dev-nixos-vm/configuration.nix
+++ system/dev-nixos-vm/configuration.nix
@@ -15,7 +15,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "dev-nixos-vm"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
