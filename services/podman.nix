156663e3d4f2a75638b84f7e774c7f7448c76639
James D <james@integratn.io>
2023-01-08 19:01:13 -0700

    improved podman

--- services/podman.nix
+++ services/podman.nix
@@ -6,12 +6,19 @@
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
+      # Make the Podman socket available in place of the Docker socket, so
+      #  Docker tools can find the Podman socket.
+      dockerSocket.enable = true;
+      extraPackages = [ pkgs.podman-compose ];
+
+      autoPrune = {
+        enable = true;
+        dates = "monthly";
+      };
 
       # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.dnsname.enable = true;
+      defaultNetwork.settings.dns_enabled = true;
     };
-    podman-compose = {
-        enable = true;
-    };
+
   };
 }
