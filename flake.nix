5cd3c9e432e5760f26da3162a78be0e4e34adbce
James D <james@integratn.io>
2023-01-09 10:53:54 -0700

    add comment on touchegg

--- flake.nix
+++ flake.nix
@@ -86,6 +86,12 @@
                 imports = [
                   ./personal.nix
                 ];
+                # Must have `services.touchegg.enable = true;` for this to work
+                # 3 Fingers UP: Present Windows
+                # 3 Fingers DOWN: Show Desktop
+                # 3 Fingers LEFT/RIGHT: Switch Virtual Desktops
+                # 4 Fingers UP/DOWN: Control System Volume
+                # [Browsers] 4 Fingers LEFT/RIGHT: Go Back/Forward
                 xdg.configFile."touchegg/touchegg.conf".source = "${toucheggkde}/touchegg.conf";
               }
             );
