38cc3ba42de6369974bd943b830bfdbb448117af
James D <james@integratn.io>
2023-01-10 10:18:51 -0700

    fixup podman

--- services/podman.nix
+++ services/podman.nix
@@ -18,7 +18,7 @@
       # };
 
       # # Required for containers under podman-compose to be able to talk to each other.
-      # defaultNetwork.settings.dns_enabled = true;
+      defaultNetwork.dnsname.enable = true;
     };
   };
 }
