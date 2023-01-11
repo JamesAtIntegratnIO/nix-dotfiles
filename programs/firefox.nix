5d6cd411553ca0efd731eefe48d9bc5ca94014e6
jamesAtIntegratnIO <james@integratn.io>
2023-01-11 10:26:53 -0700

    get firefox addons working

--- programs/firefox.nix
+++ programs/firefox.nix
@@ -1,10 +1,16 @@
 { config, pkgs, withGUI, ... }:
 {
-    programs.firefox = {
-        enable = withGUI;
-        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-            onepassword-password-manager
-            ublock-origin
-        ];
+  programs.firefox = {
+    enable = withGUI;
+    extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      onepassword-password-manager
+      ublock-origin
+    ];
+    profiles.default = {
+      id = 0;
+      name = "James";
+      isDefault = true;
     };
+  };
+  
 }
