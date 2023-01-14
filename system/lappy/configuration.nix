b61170155a9dc0938f97ad841f1e5bdc5a503f7a
jamesAtIntegratnIO <james@integratn.io>
2023-01-14 15:32:21 -0700

    rearranged configuration

--- system/lappy/configuration.nix
+++ system/lappy/configuration.nix
@@ -5,12 +5,13 @@
 { config, lib, inputs, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./borgmatic.nix
-      ./logrotate.nix
-    ];
+  imports = [ 
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./borgmatic.nix
+    ./logrotate.nix
+  ];
+
   boot = {
     kernelPackages = pkgs.linuxPackages_6_1;
     # Bootloader.
@@ -28,6 +29,48 @@
       "/crypto_keyfile.bin" = null;
     };
   };
+    environment = {
+    variables = {
+      WEBKIT_DISABLE_COMPOSITING_MODE = "1";
+    };
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    systemPackages = with pkgs; [
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
+      wally-cli
+      libnotify
+      cups-pdf-to-pdf
+    ];
+  };
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  };
+
+  hardware = {
+    # For zsa keyboards
+    keyboard.zsa.enable = true;
+    # Will not work if pipewire is enabled (prefer pipewire)
+    pulseaudio.enable = false;
+    # Enable bluetooth hardware
+    bluetooth.enable = true;
+    # Enable cause sound don't work
+    enableAllFirmware = true;
+  };
+
   networking = {
     # Define your hostname
     hostName = "lappy";
@@ -48,81 +91,72 @@
     };
   };
 
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  
-
-  # Set your time zone.
-  time.timeZone = "America/Denver";
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  security = {
+    # Yubikey
+    pam.services = {
+      login.u2fAuth = true;
+      sudo.u2fAuth = true;
+    };
+    rtkit.enable = true;
+  };  
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+  services = {
+    logind = {
+      lidSwitchExternalPower = "ignore";
+    };
+    xserver = {
+      # Enable the X11 windowing system.
+      enable = true;
+      # Enable the KDE Plasma Desktop Environment.
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+      # Configure keymap in X11
+      layout = "us";
+      xkbVariant = "";
+    };
+    # Enable touchegg
+    touchegg.enable = true;
+    # Disabled because fprint is forced even if the laptop is closed
+    # Enable the fingerprint reader
+    # services.fprintd.enable = true;
+    # services.fprintd.tod.enable = true;
+    # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090;
+
+    # Enable CUPS to print documents.
+    printing = {
+      enable = true;
+      drivers = [ pkgs.mfcl3770cdwlpr pkgs.mfcl3770cdwcupswrapper ];
+    };
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+    # Enable the OpenSSH daemon.
+    openssh.enable = true;
+
+  # Yubikey
+    udev.packages = [ pkgs.yubikey-personalization ];
+    pcscd.enable = true;
+  
   };
 
-  services.xserver = {
-    # Enable the X11 windowing system.
-    enable = true;
-    # Enable the KDE Plasma Desktop Environment.
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
-    # Configure keymap in X11
-    layout = "us";
-    xkbVariant = "";
-  };
-  # Enable touchegg
-  services.touchegg.enable = true;
-
-  # For zsa keyboards
-  hardware.keyboard.zsa.enable = true;
-
-  # Disabled because fprint is forced even if the laptop is closed
-  # Enable the fingerprint reader
-  # services.fprintd.enable = true;
-  # services.fprintd.tod.enable = true;
-  # services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090;
-
-  # Enable CUPS to print documents.
-  services.printing = {
-    enable = true;
-    drivers = [ pkgs.brlaser ];
-  };
-
-  # Enable bluetooth hardware
-  hardware.bluetooth.enable = true;
-
-  # Enable cause sound don't work
-  hardware.enableAllFirmware = true;
   # Enable sound with pipewire.
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+  
+  # Set your time zone.
+  time.timeZone = "America/Denver";
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -135,21 +169,10 @@
     packages = with pkgs; [
       plex-media-player
       slack
-    #  thunderbird
     ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-    wally-cli
-    libnotify
-  ];
+  
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -159,24 +182,6 @@
     enableSSHSupport = true;
   };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Yubikey
-  services.udev.packages = [ pkgs.yubikey-personalization ];
-  services.pcscd.enable = true;
-  security.pam.services = {
-    login.u2fAuth = true;
-    sudo.u2fAuth = true;
-  };
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- system/lappy/logrotate.nix
+++ system/lappy/logrotate.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 {
   services.logrotate = {
-    enable = true;
+    enable = false;
     settings = {
       borgmatic = {
         files = [
