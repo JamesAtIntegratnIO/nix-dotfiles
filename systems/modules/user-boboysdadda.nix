5887924abe8dd84ff85022c43b169cbeb11d6b33
jamesAtIntegratnIO <james@integratn.io>
2023-01-15 15:07:23 -0700

    move home-manager

--- systems/modules/user-boboysdadda.nix
+++ systems/modules/user-boboysdadda.nix
@@ -14,7 +14,5 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMUgouRGqNgbaBlyGh2hx+rZB72ev7DtcStA3vD9ziZ"
     ];
-    # changeme on first boot or get rekt. Really need to learn age/ragenix
-    initialPassword = "changeme";
   };
 }
