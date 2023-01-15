786c12cf950b6352ac9c4e203cbb8010a6e1d524
jamesAtIntegratnIO <james@integratn.io>
2023-01-15 15:24:51 -0700

    this doesn't work

--- systems/default.nix
+++ systems/default.nix
@@ -41,7 +41,7 @@
         ./modules/tailscale.nix
         ./modules/i18n.nix
         ./modules/openssh.nix
-        ./modules/root-user.nix
+        ./modules/user-root.nix
       ];
     })
   ];
@@ -162,6 +162,33 @@ in {
           }: {
             services.vscode-server.enable = true;
           })
+          # home-manager.nixosModules.home-manager
+          # ({specialArgs, ...}: {
+          #   home-manager.useGlobalPkgs = true;
+          #   home-manager.useUserPackages = true;
+          #   home-manager.extraSpecialArgs = specialArgs;
+          #   home-manager.users.boboysdadda = (
+          #     {
+          #       config,
+          #       pkgs,
+          #       extraSpecialArgs,
+          #       ...
+          #     }: {
+          #       home.stateVersion = "20.09";
+          #       targets.genericLinux.enable = true;
+          #       imports = [
+          #         ../personal.nix
+          #       ];
+          #       # Must have `services.touchegg.enable = true;` for this to work
+          #       # 3 Fingers UP: Present Windows
+          #       # 3 Fingers DOWN: Show Desktop
+          #       # 3 Fingers LEFT/RIGHT: Switch Virtual Desktops
+          #       # 4 Fingers UP/DOWN: Control System Volume
+          #       # [Browsers] 4 Fingers LEFT/RIGHT: Go Back/Forward
+          #       xdg.configFile."touchegg/touchegg.conf".source = "${toucheggkde}/touchegg.conf";
+          #     }
+          #   );
+          # })
         ];
     };
   };
--- systems/modules/home-manager.nix
+++ systems/modules/home-manager.nix
@@ -1,29 +1,33 @@
-{ config, pkgs, specialArgs, ... }: {
-  home-manager.nixosModules.home-manager
-  ({specialArgs, ...}: {
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-    home-manager.extraSpecialArgs = specialArgs;
-    home-manager.users.boboysdadda = (
-      {
-        config,
-        pkgs,
-        extraSpecialArgs,
-        ...
-      }: {
-        home.stateVersion = "20.09";
-        targets.genericLinux.enable = true;
-        imports = [
-          ../personal.nix
-        ];
-        # Must have `services.touchegg.enable = true;` for this to work
-        # 3 Fingers UP: Present Windows
-        # 3 Fingers DOWN: Show Desktop
-        # 3 Fingers LEFT/RIGHT: Switch Virtual Desktops
-        # 4 Fingers UP/DOWN: Control System Volume
-        # [Browsers] 4 Fingers LEFT/RIGHT: Go Back/Forward
-        xdg.configFile."touchegg/touchegg.conf".source = "${toucheggkde}/touchegg.conf";
-      }
-    );
-  })
-}
+{
+  config,
+  pkgs,
+  specialArgs,
+  home-manager,
+  ...
+}:
+home-manager.nixosModules.home-manager
+({specialArgs, ...}: {
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.extraSpecialArgs = specialArgs;
+  home-manager.users.boboysdadda = (
+    {
+      config,
+      pkgs,
+      extraSpecialArgs,
+    }: {
+      home.stateVersion = "20.09";
+      targets.genericLinux.enable = true;
+      imports = [
+        ../../personal.nix
+      ];
+      # Must have `services.touchegg.enable = true;` for this to work
+      # 3 Fingers UP: Present Windows
+      # 3 Fingers DOWN: Show Desktop
+      # 3 Fingers LEFT/RIGHT: Switch Virtual Desktops
+      # 4 Fingers UP/DOWN: Control System Volume
+      # [Browsers] 4 Fingers LEFT/RIGHT: Go Back/Forward
+      # xdg.configFile."touchegg/touchegg.conf".source = "${toucheggkde}/touchegg.conf";
+    }
+  );
+})
