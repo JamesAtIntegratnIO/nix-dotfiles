664b941a23fcc661b81239670a71633454817ae5
jamesAtIntegratnIO <james@integratn.io>
2023-04-07 14:32:42 -0600

    add git alias

--- systems/home-manager-modules/git.nix
+++ systems/home-manager-modules/git.nix
@@ -5,10 +5,14 @@
     userEmail = "james@integratn.io";
     aliases = {
       st = "status";
+      branches = "for-each-ref --sort=-committerdate refs/heads/ --format='%(authordate:short) %(color:red)%(objectname:short) %(color:yellow)%(refname:short)%(color:reset) (%(color:green)%(committerdate:relative)%(color:reset))'";
     };
     extraConfig = {
       global = {
         autoSetupRemote = true;
+        init = {
+          defaultBranch = "main";
+        }
       };
     };
   };
